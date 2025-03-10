1fc7e1191336620b6ab50eeee549cd9cb9b210a9
Kwkwkwjwowj <136041487+Kwkwkwjwowj@users.noreply.github.com>
2025-03-11 03:01:03 +0800

    Memperbaiki masalah timeout

--- searchgroups.js
+++ searchgroups.js
@@ -9,7 +9,7 @@ app.set('json spaces', 2);
 async function cariGC(q) {
     try {
         const { data } = await axios.get(`https://groupsor.link/group/searchmore/${q.replace(/\s+/g, '-')}`, {
-            timeout: 15000
+            timeout: 30000
         });
 
         const $ = cheerio.load(data);
